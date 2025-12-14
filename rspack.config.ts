14cc59da2c1361be4555561784f2ab3acc34783b
Aleksei Batukhtin <full.naked.eye@gmail.com>
2025-12-14 20:03:42 +0500

    init scene

---
+++ rspack.config.ts
@@ -0,0 +1,72 @@
+import { defineConfig } from "@rspack/cli";
+import { rspack, type SwcLoaderOptions } from "@rspack/core";
+import path from 'path';
+// Target browsers, see: https://github.com/browserslist/browserslist
+const targets = ["last 2 versions", "> 0.2%", "not dead", "Firefox ESR"];
+
+export default defineConfig({
+	entry: {
+		main: "./src/index.ts"
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+	},
+	resolve: {
+		extensions: ["...", ".ts"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.svg$/,
+				type: "asset"
+			},
+			{
+				test: /\.js$/,
+				use: [
+					{
+						loader: "builtin:swc-loader",
+						options: {
+							jsc: {
+								parser: {
+									syntax: "ecmascript"
+								}
+							},
+							env: { targets }
+						} satisfies SwcLoaderOptions
+					}
+				]
+			},
+			{
+				test: /\.ts$/,
+				use: [
+					{
+						loader: "builtin:swc-loader",
+						options: {
+							jsc: {
+								parser: {
+									syntax: "typescript"
+								}
+							},
+							env: { targets }
+						} satisfies SwcLoaderOptions
+					}
+				]
+			}
+		]
+	},
+	plugins: [new rspack.HtmlRspackPlugin({ template: "./dist/index.html" })],
+	optimization: {
+		minimizer: [
+			new rspack.SwcJsMinimizerRspackPlugin(),
+			new rspack.LightningCssMinimizerRspackPlugin({
+				minimizerOptions: { targets }
+			})
+		]
+	},
+	devServer: {
+		port: 3000
+	},
+	experiments: {
+		css: true
+	}
+});
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import 'reset.css';
+
+import { createScene } from './createScene';
+
+const container = document.body;
+
+createScene(container!);
