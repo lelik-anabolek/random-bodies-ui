42bdb1186cafddb8dd318e6ec9c2cfaaf041c0e5
Aleksei Batukhtin <full.naked.eye@gmail.com>
2026-06-03 22:08:13 +0500

    feat: velocity, position, mass, radius inputs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import 'reset.css';
 
 import { createScene } from './createScene';
 
-const container = document.body;
+const container = document.getElementById('scene');
 
 createScene(container!);
